a974d5c82a50d37cf72a1110f0bb8fdc06113c10
Gabriel Louzada Rascovit <rascovit@gmail.com>
2017-02-21 00:07:07 -0300

    create initial structure for the item model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates_presence_of :gender
   validates_presence_of :latitude
   validates_presence_of :longitude
+
+  has_many :items
 end
---
+++ spec/factories/items.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :item do
+    name { 'water' }
+    user nil
+  end
+end
