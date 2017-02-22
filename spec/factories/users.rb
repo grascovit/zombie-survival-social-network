9a06b2189c312a519560f1157577f1d92d28e2fd
Gabriel Louzada Rascovit <rascovit@gmail.com>
2017-02-22 18:31:25 -0300

    create factory for infected user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -14,4 +14,13 @@ FactoryGirl.define do
     latitude nil
     longitude nil
   end
+
+  factory :infected_user, parent: :user do
+    name { Faker::Name.name }
+    age { Faker::Number.number(2) }
+    gender { Faker::Lorem.word }
+    latitude { Faker::Number.decimal(2, 6) }
+    longitude { Faker::Number.decimal(2, 6) }
+    received_infection_alerts { create_list(:infection_alert, 3) }
+  end
 end
