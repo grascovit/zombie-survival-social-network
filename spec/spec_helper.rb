fb84b1e8ee2f3926e046c220b533fd31c54696f9
Gabriel Rascovit <gabrielrascovit@zgsolucoes.com.br>
2017-02-23 14:00:48 -0300

    ignore channels, jobs and mailers from coverage report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,11 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_filter '/channels'
+  add_filter '/jobs'
+  add_filter '/mailers'
+end
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
